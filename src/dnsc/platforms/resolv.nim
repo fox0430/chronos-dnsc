a16caf301f6581481a6ac1e24707c91939b7e7c7
fox0430 <shuu.n@protonmail.com>
2026-04-03 16:18:11 +0900

    Expand resolv.conf parser to support all major directives

--- src/dnsc/platforms/resolv.nim
+++ src/dnsc/platforms/resolv.nim
@@ -4,12 +4,11 @@
 ## Minimal implementation to get System DNS Server (IPv4 and IPv6). This implementation is heavily
 ## influenced by glibc.
 ##
-## Implements a parser to capture the first nameserver in the resolver configuration file, which is,
-## by default, /etc/resolv.conf. You can change this file by passing to compile
-## `-d:dnscPathResConf=/etc/myresolv.conf`.
+## Implements a parser for the resolver configuration file, which is, by default, /etc/resolv.conf.
+## You can change this file by passing to compile `-d:dnscPathResConf=/etc/myresolv.conf`.
 ##
-## Checking for changes in the `dnscPathResConf` file performed at each new `getSystemDnsServer()`
-## call makes the code 2x faster, if there is no change.
+## Checking for changes in the `dnscPathResConf` file performed at each new call makes the code 2x
+## faster, if there is no change.
 ##
 ## This implementation should work for systems that adopt resolver. Currently this implementation is
 ## imported into Linux and BSD. If your platform uses a resolver configuration file, compile with
@@ -17,7 +16,15 @@
 ##
 ## References:
 ## - https://man7.org/linux/man-pages/man5/resolv.conf.5.html
-import std/[os, parseutils, times]
+import std/[os, parseutils, strutils, times]
+
+const
+  dnscPathResConf* {.strdefine.} = "/etc/resolv.conf"
+    ## Resolver configuration file. You can change by compiling with
+    ## `-d:dnscPathResConf=/etc/myresolv.conf`.
+
+  MAXNS = 3 ## Maximum number of nameservers
+  MAXDNSRCH = 6 ## Maximum number of search domains
 
 type
   FileChangeDetection = object ## Object for `dnscPathResConf` file information.
@@ -26,17 +33,22 @@ type
     lastWriteTime: Time ## Last write time
     creationTime: Time ## Creation time
 
+  ResolvConf* = object ## Parsed resolver configuration.
+    nameservers*: seq[string] ## DNS server IPs (up to MAXNS).
+    domain*: string ## Local domain name.
+    search*: seq[string] ## Search list for hostname lookup (up to MAXDNSRCH).
+    ndots*: int
+      ## Threshold for number of dots before initial absolute query (default: 1).
+    timeout*: int ## Timeout in seconds for queries (default: 5).
+    attempts*: int ## Number of query attempts (default: 2).
+    rotate*: bool ## Round-robin selection of nameservers.
+
   ResolvConfGlobal = object ## Object for global resolver information.
-    nameserver: string
-      ## The first nameserver caught in the last parse of `dnscPathResConf`
+    resolvConf: ResolvConf ## Parsed configuration from last parse.
     fileResolvConf: FileChangeDetection
       ## `dnscPathResConf` file information during last parse.
     initialized: bool
-      ## Determines whether the `dnscPathResConf` file has already been parsed
-
-const dnscPathResConf* {.strdefine.} = "/etc/resolv.conf"
-  ## Resolver configuration file. You can change by compiling with
-  ## `-d:dnscPathResConf=/etc/myresolv.conf`.
+      ## Determines whether the `dnscPathResConf` file has already been parsed.
 
 var resolvGlobal {.threadvar.}: ResolvConfGlobal
   ## Keeps information from the `dnscPathResConf` file and if it has already been parsed.
@@ -49,6 +61,150 @@ proc isUnchanged(fileInfo: FileInfo): bool =
     (fileInfo.creationTime == resolvGlobal.fileResolvConf.creationTime) and
     (fileInfo.lastWriteTime == resolvGlobal.fileResolvConf.lastWriteTime)
 
+proc initResolvConf*(): ResolvConf =
+  ## Returns a ResolvConf with default values.
+  result.ndots = 1
+  result.timeout = 5
+  result.attempts = 2
+
+proc parseIntOption(value: string, minVal: int): int =
+  ## Parses an integer option value. Returns the parsed value if valid and >= minVal,
+  ## otherwise returns -1.
+  try:
+    let v = parseInt(value)
+    if v >= minVal: v else: -1
+  except ValueError:
+    -1
+
+proc parseOptions(result: var ResolvConf, line: string, start: int) =
+  ## Parses the options directive. Options are space-separated and can be
+  ## `key:value` or just `key`.
+  var pos = start
+
+  while pos < line.len:
+    pos += skipWhitespace(line, pos)
+    if pos >= line.len:
+      break
+
+    var opt: string
+    let count =
+      parseUntil(line, opt, {' ', '\t', '\v', '\r', '\n', '\f', ';', '#'}, pos)
+    if count == 0:
+      break
+    pos += count
+
+    let colonIdx = opt.find(':')
+    if colonIdx >= 0:
+      let
+        key = opt[0 ..< colonIdx]
+        value = opt[colonIdx + 1 .. ^1]
+      case key
+      of "ndots":
+        let v = parseIntOption(value, 0)
+        if v >= 0:
+          result.ndots = v
+      of "timeout":
+        let v = parseIntOption(value, 1)
+        if v >= 0:
+          result.timeout = v
+      of "attempts":
+        let v = parseIntOption(value, 1)
+        if v >= 0:
+          result.attempts = v
+      else:
+        discard
+    else:
+      case opt
+      of "rotate":
+        result.rotate = true
+      else:
+        discard
+
+proc parseResolvConf*(content: string): ResolvConf =
+  ## Parses resolv.conf content and returns a ResolvConf.
+  ##
+  ## Parses the following directives:
+  ## - `nameserver` (up to 3)
+  ## - `domain`
+  ## - `search` (up to 6 domains)
+  ## - `options` (ndots, timeout, attempts, rotate)
+  ##
+  ## `domain` and `search` are mutually exclusive; the last one in the file takes effect.
+  const
+    comments = {';', '#'}
+    whiteSpaces = {' ', '\t', '\v', '\r', '\n', '\f'}
+    commentsAndWhiteSpaces = comments + whiteSpaces
+
+  result = initResolvConf()
+
+  for line in content.splitLines():
+    if line == "":
+      continue
+    if line[0] in comments:
+      continue
+
+    var strConf: string
+    let count = parseUntil(line, strConf, whiteSpaces)
+
+    if count > 0:
+      let valueStart = count + skipWhitespace(line, count)
+      case strConf
+      of "nameserver":
+        if result.nameservers.len < MAXNS:
+          var ns: string
+          if parseUntil(line, ns, commentsAndWhiteSpaces, valueStart) > 0:
+            result.nameservers.add(ns)
+      of "domain":
+        var dom: string
+        if parseUntil(line, dom, commentsAndWhiteSpaces, valueStart) > 0:
+          result.domain = dom
+          result.search = @[]
+      of "search":
+        result.search = @[]
+        result.domain = ""
+        var pos = valueStart
+        while pos < line.len and result.search.len < MAXDNSRCH:
+          pos += skipWhitespace(line, pos)
+          if pos >= line.len or line[pos] in comments:
+            break
+          var dom: string
+          let c = parseUntil(line, dom, commentsAndWhiteSpaces, pos)
+          if c > 0:
+            result.search.add(dom)
+            pos += c
+          else:
+            break
+      of "options":
+        result.parseOptions(line, valueStart)
+      else:
+        discard
+
+proc getResolvConf*(): ResolvConf =
+  ## Returns the parsed resolver configuration from the `dnscPathResConf` file.
+  ##
+  ## The result is cached based on file metadata (ID, size, timestamps). If the file has not
+  ## changed since the last call, the cached result is returned immediately.
+  var fileInfo: FileInfo
+  try:
+    fileInfo = getFileInfo(dnscPathResConf)
+  except OSError:
+    return initResolvConf()
+
+  if resolvGlobal.initialized and fileInfo.isUnchanged():
+    return resolvGlobal.resolvConf
+
+  try:
+    result = parseResolvConf(readFile(dnscPathResConf))
+  except IOError:
+    result = initResolvConf()
+
+  resolvGlobal.resolvConf = result
+  resolvGlobal.fileResolvConf.fileId = fileInfo.id.file
+  resolvGlobal.fileResolvConf.size = fileInfo.size
+  resolvGlobal.fileResolvConf.creationTime = fileInfo.creationTime
+  resolvGlobal.fileResolvConf.lastWriteTime = fileInfo.lastWriteTime
+  resolvGlobal.initialized = true
+
 proc getSystemDnsServer*(): string =
   ## Returns the first nameserver found in the `dnscPathResConf` file. Will return `""` if not
   ## found.
@@ -57,225 +213,6 @@ proc getSystemDnsServer*(): string =
   ## changed since the last call, the cached result is returned immediately. Note that an empty
   ## string result is also cached — if no nameserver is found, subsequent calls will return `""`
   ## until the file changes.
-  const
-    comments = {';', '#'}
-    whiteSpaces = {' ', '\t', '\v', '\r', '\n', '\f'}
-    commentsAndWhiteSpaces = comments + whiteSpaces
-
-  var fileInfo: FileInfo
-  try:
-    fileInfo = getFileInfo(dnscPathResConf)
-  except OSError:
-    return
-
-  if resolvGlobal.initialized and fileInfo.isUnchanged():
-    result = resolvGlobal.nameserver
-  else:
-    try:
-      for line in lines(dnscPathResConf):
-        if line == "":
-          continue # skip empty line
-        if line[0] in comments:
-          continue # skip comments
-
-        var strConf: string
-
-        let count = parseUntil(line, strConf, whiteSpaces)
-
-        if count > 0:
-          case strConf
-          of "nameserver":
-            if parseUntil(
-              line, result, commentsAndWhiteSpaces, count + skipWhitespace(line, count)
-            ) > 0:
-              break
-          else:
-            # for now there is no interest in implementing: domain, search and options
-            discard
-    except IOError:
-      return
-
-    resolvGlobal.nameserver = result
-    resolvGlobal.fileResolvConf.fileId = fileInfo.id.file
-    resolvGlobal.fileResolvConf.size = fileInfo.size
-    resolvGlobal.fileResolvConf.creationTime = fileInfo.creationTime
-    resolvGlobal.fileResolvConf.lastWriteTime = fileInfo.lastWriteTime
-    resolvGlobal.initialized = true
-
-when false:
-  # Discontinued implementation. Reasons:
-  # - Systems based on musl libc do not have `res_init()` implemented;
-  # - Different implementations of resolv.h.
-  # - Operating systems using deprecated resolv implementation
-  # - OpenBSD has its own `struct __res_state`
-
-  ## Minimal implementation to get System DNS Server (IPv4 only)
-  ##
-  ## This implementation uses the resolv library, which should work on Linux and
-  ## BSD.
-  ##
-  ## References:
-  ## - https://man7.org/linux/man-pages/man3/resolver.3.html
-  ## - https://www.freebsd.org/cgi/man.cgi?query=resolver
-  ##
-  ## Using this module implies passing `-lresolv` or `-lc` to the linkage process.
-  ##
-  ## Notes:
-  ## - To use the interface deprecated by the resolv library, compile with
-  ##   `-d:useDeprecatedResolv`. On **OpenBSD** it is recommended to define this symbol
-  ##   when compiling, since the `resolv.h` used on this platform has its own
-  ##   definitions that will only be used when this symbol is defined.
-  ## - Unfortunately systems based on musl libc do not have `res_init()`
-  ##   implemented. Such a libc loads the settings from "/etc/resolv.conf", when
-  ##   needed, through `__get_resolv_conf()` which is not compatible with
-  ##   `struct __res_state`. Faced with so many divergences found using
-  ##   `resolv.h`, I believe it is better to implement a parser for
-  ##   "/etc/resolv.conf". TODO
-  when defined(nimdoc):
-    import std/[net, winlean]
-  else:
-    import std/[net, posix]
-
-  when defined(bsd):
-    {.passL: "-lc".}
-  else:
-    {.passL: "-lresolv".}
-
-  const
-    useOpenBSDResolv =
-      when defined(useDeprecatedResolv) and defined(openbsd): true else: false
-      # The structure of res_state in OpenBSD has several peculiarities, as well
-      # as currently adopts the deprecated version with res_init().
-      # https://github.com/openbsd/src/blob/e3c5fa921ef394179421471c88eb2be26d8a6692/include/resolv.h
-
-    MAXNS = 3
-    MAXDNSRCH = 6
-    MAXRESOLVSORT = 10
-
-    RES_INIT = 1
-
-  when useOpenBSDResolv:
-    {.
-      emit: """/*INCLUDESECTION*/
-  #include <netinet/in.h>
-  """
-    .} # See https://github.com/troglobit/inadyn/issues/241
-
-    const MAXDNSLUS = 4
-
-    type
-      CulongOrCuint = cuint
-
-      ResTimeSpecObj = object
-        resSec: Time
-        resNSec: clong
-
-  else:
-    type CulongOrCuint = culong
-
-  type
-    # Commented for being currently in disuse
-    #ResSendhookact {.size: 4.} = enum
-    #  ResGoahead, ResNextns, ResModified, ResDone, ResError
-
-    #ResSendQhook = proc (ns: ptr ptr Sockaddr_in, query: ptr ptr uint8,
-    #                     querylen: ptr cint, ans: ptr uint8, anssiz: cint,
-    #                     resplen: ptr cint): ResSendhookact {.cdecl.}
-
-    #ResSendRhook = proc (ns: ptr Sockaddr_in, query: ptr uint8, querylen: cint,
-    #                     ans: ptr uint8, anssiz: cint, resplen: ptr cint): ResSendhookact {.cdecl.}
-    ResSendQhook = pointer
-    ResSendRhook = pointer
-
-    SortListObj = object
-      `addr`: InAddr
-      mask: uint32 # uint32_t
-
-    Ext = object
-      nscount: uint16 # u_int16_t
-      nsmap: array[MAXNS, uint16]
-      nssocks: array[MAXNS, cint]
-      nscount6: uint16
-      nsinit: uint16
-      nsaddrs: array[MAXNS, ptr Sockaddr_in6]
-      initstamp: array[2, cuint]
-
-    UUnion {.union.} = object
-      pad: array[52, cchar]
-      ext: Ext
-
-    ResState = object
-      retrans: cint
-      retry: cint
-      options: CulongOrCuint
-      nscount: cint
-      when useOpenBSDResolv:
-        family: array[2, cint]
-      nsaddrList: array[MAXNS, Sockaddr_in]
-      id: cushort
-      dnsrch: array[MAXDNSRCH + 1, cstring]
-      defdname: array[256, cchar]
-      pfcode: CulongOrCuint
-      ndots {.bitsize: 4.}: cuint
-      nsort {.bitsize: 4.}: cuint
-      when useOpenBSDResolv:
-        unused: array[3, cchar]
-      else:
-        ipv6_unavail {.bitsize: 1.}: cuint
-        unused {.bitsize: 23.}: cuint
-      sortList: array[MAXRESOLVSORT, SortListObj]
-      when useOpenBSDResolv:
-        lookups: array[MAXDNSLUS, cchar]
-        restimespec: ResTimeSpecObj
-        reschktime: Time
-      else:
-        qhook: ResSendQhook
-        rhook: ResSendRhook
-        resHErrno: cint
-        vcsock: cint
-        flags: cuint
-        u: UUnion
-
-  {.push header: "<resolv.h>".}
-  type SResState {.importc: "struct __res_state".} = object
-
-  when not defined(useDeprecatedResolv):
-    proc resNInit(statep: ptr SResState): cint {.importc: "res_ninit".}
-    proc resNClose(rstatep: ptr SResState) {.importc: "res_nclose".}
-  else:
-    when useOpenBSDResolv:
-      var res {.importc: "_res".}: SResState
-        # currently it is a C macro for `struct __res_state __res_state(void)`
-    else:
-      proc resState(): ptr SResState {.importc: "__res_state".}
-
-    proc resInit(): cint {.importc: "res_init".}
-  {.pop.}
-
-  proc getSystemDnsServer*(): string =
-    ## Returns the IPv4 used by the system for DNS resolution. Otherwise it
-    ## returns an empty string `""`.
-    var
-      rs: ResState
-      ip: IpAddress
-      port: Port
-      rInit =
-        when defined(useDeprecatedResolv):
-          resInit()
-        else:
-          resNInit(cast[ptr SResState](addr rs))
-
-    if rInit == 0:
-      when useOpenBSDResolv:
-        rs = cast[ResState](res)
-      elif defined(useDeprecatedResolv):
-        rs = cast[ResState](resState()[])
-          # If nim compiled with `--threads:on`, on NetBSD it will result in SIGABRT
-
-      if (rs.options and RES_INIT) == RES_INIT:
-        fromSockAddr(rs.nsaddrList[0], sizeof(Sockaddr_in).SockLen, ip, port)
-
-        result = $ip
-
-      when not defined(useDeprecatedResolv):
-        resNClose(cast[ptr SResState](addr rs))
+  let conf = getResolvConf()
+  if conf.nameservers.len > 0:
+    result = conf.nameservers[0]
